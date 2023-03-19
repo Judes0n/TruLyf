c0a40ea4ee219f8c1eaa60118b34c168f6fd342e
Judes0n <judesonjoy111@gmail.com>
2023-03-19 18:34:00 +0530

    Daily Commit

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -7,22 +7,61 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit{
   role : string;
+  homeRoute: string;
+  profileRoute: string;
+  isLoggedIn : boolean;
   constructor(private router : Router)
   {
 
   }
-  ngOnInit()
-  {
-    this.role = this.readSession('log_role');
+  ngOnInit() {
+    this.AuthUser();
   }
 
   readSession(key : string) : string
   {
     return sessionStorage.getItem(key);
   }
+  AuthUser() : boolean
+  {
+    this.role = this.readSession('log_role');
+    this.SetHomeRoute();
+    if(this.role!=null)
+      return false;
+    return true;
+  }
+  SetHomeRoute()
+  {
+    this.isLoggedIn = this.role === null ? false : true;
+    switch (this.role) {
+      case 'client': this.homeRoute = '/Home/Client';
+        break;
+      case 'agent': this.homeRoute = '/Home/Agent';
+        break;
+      case 'company': this.homeRoute = '/Home/Company';
+        break;
+      case 'admin': this.homeRoute = '/Home/Admin';
+        break;
+      default: this.homeRoute = '/Home'
+    }
+  }
+  SetProfileRoute()
+  {
+    this.isLoggedIn = this.role === null ? false : true;
+    switch (this.role) {
+      case 'client': this.profileRoute = '/Client/Profile';
+        break;
+      case 'agent': this.profileRoute = '/Agent/Profile';
+        break;
+      case 'company': this.profileRoute = '/Company/Profile';
+        break;
+      case 'admin': this.profileRoute = '/Admin/Profile';
+        break;
+      default: this.profileRoute = '/Error'
+    }
+  }
   logout()
   {
     sessionStorage.removeItem('log_role');
-    this.router.navigate(['\Home']);
   }
 }
