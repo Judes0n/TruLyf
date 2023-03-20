dc798cd0155c13b867f6213b51adf7e54a886585
Judes0n <judesonjoy111@gmail.com>
2023-03-20 17:18:43 +0530

    Register & Login Functions

--- src/app/services/User/user.service.ts
+++ src/app/services/User/user.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { StatusEnum } from 'src/app/enum/user-status-enum';
 import { UserTypeEnum } from 'src/app/enum/user-type-enum';
 import { User } from 'src/app/models/user';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -10,4 +12,12 @@ import { User } from 'src/app/models/user';
 export class UserService {
 
   constructor(private http : HttpClient) {}
+
+  register(userreq : User) : Observable<User>{
+    return this.http.post<User>(environment.baseApiUrl+'/api/User/Register',userreq);
+  }
+
+  login(userreq : User) : Observable<User>{
+    return this.http.post<User>(environment.baseApiUrl+'/api/User/Login',userreq);
+  }
 }
