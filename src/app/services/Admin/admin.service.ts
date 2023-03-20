45462e0a906c35dc531bbfff803a8c0017f81fe0
Judes0n <judesonjoy111@gmail.com>
2023-03-20 16:51:18 +0530

    20/03 Daily Commit

--- src/app/services/Admin/admin.service.ts
+++ src/app/services/Admin/admin.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
-  baseApiUrl : string ="https://localhost:7037";
+  baseApiUrl : string = environment.baseApiUrl;
   constructor(private http : HttpClient) { }
   getAllUsers() : Observable<User[]> {
    return this.http.get<User[]>(this.baseApiUrl+'/api/User/GetAllUsers');
