1a1194ec1a7d2fa797fd360d05018a495a749680
Judes0n <judesonjoy111@gmail.com>
2023-04-29 17:59:11 +0530

    profile component modified

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { StatusEnum } from 'src/app/enum/user-status-enum';
+import { Agent } from 'src/app/models/agent';
+import { Client } from 'src/app/models/client';
+import { Company } from 'src/app/models/company';
+import { User } from 'src/app/models/user';
+import { AgentService } from 'src/app/services/Agent/agent.service';
+import { ClientService } from 'src/app/services/Client/client.service';
+import { CompanyService } from 'src/app/services/Company/company.service';
+import { UserService } from 'src/app/services/User/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,12 +18,53 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-  private userid : number;
+  userid: number;
+  usertype: string;
+  isEditable: boolean;
+  profileuser: User;
+  profileForm: FormGroup = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required)
+  });
 
- constructor(private route : ActivatedRoute) { }
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private userservice: UserService, private companyservice: CompanyService, private agentservice: AgentService, private clientservice: ClientService) { }
+
+  ngOnInit() {
+    this.isEditable = false;
     this.route.params.subscribe(params => {
       this.userid = params['userid'];
     });
+    this.usertype = this.readSession('log_role');
+    this.userservice.GetUser(this.userid).subscribe(u => {
+      this.profileuser = u;
+      this.setForm(u);
+    });
+  }
+
+  setForm(user: User) {
+    this.profileForm.get('username').setValue(user.userName);
+    this.profileForm.get('password').setValue(user.password);
+  }
+
+  readSession(key: string): string {
+    return sessionStorage.getItem(key);
+  }
+
+  Toggle() {
+    this.isEditable = !this.isEditable;
+  }
+
+  subProfile() {
+    let user: User = {
+      userId: this.profileuser.userId,
+      userName: this.profileForm.get('username').value,
+      password: this.profileForm.get('password').value,
+      status: this.profileuser.status,
+      type: this.profileuser.type
+    };
+    this.userservice.UpdateUser(user).subscribe(res => {
+      alert("Profile Updated!!");
+    });
+    this.ngOnInit();
   }
 }
--- src/app/services/User/user.service.ts
+++ src/app/services/User/user.service.ts
@@ -19,9 +19,8 @@ export class UserService {
     const formData = new FormData();
     formData.append('pic', file);
     formData.append('email', email);
-    if(userreq.type != 1)
-    {
-    formData.append('gender', gender);
+    if (userreq.type != 1) {
+      formData.append('gender', gender);
     }
     for (const key in userreq) {
       if (userreq.hasOwnProperty(key)) {
@@ -29,8 +28,8 @@ export class UserService {
       }
     }
     this.http.post(environment.baseApiUrl + '/api/User/Register', formData).subscribe(
-      (response : User) => {
-        alert(response.userName+" Registered Sucessfully");
+      (response: User) => {
+        alert(response.userName + " Registered Sucessfully");
         this.router.navigate(['/Login']);
       }
     );
@@ -43,19 +42,19 @@ export class UserService {
       console.log("Request Data is Faulty");
       return;
     }
-    this.http.post<User>(environment.baseApiUrl + '/api/User/Login', userreq).subscribe((response : User) => {
+    this.http.post<User>(environment.baseApiUrl + '/api/User/Login', userreq).subscribe((response: User) => {
       if (response == null) {
         alert('Fetch Failed');
       }
       else {
         console.log(response);
-         if (response.type == UserTypeEnum.Admin) {
+        if (response.type == UserTypeEnum.Admin) {
           sessionStorage.setItem('log_role', 'admin');
           sessionStorage.setItem('userID', response.userId.toString());
           setTimeout(() => {
             this.router.navigate(['/Home/Admin']);
           });
-         }
+        }
         if (response.status == StatusEnum.Active) {
           switch (response.type) {
             case UserTypeEnum.Client: {
@@ -106,27 +105,35 @@ export class UserService {
     return dbpath;
   }
 
-  GetUserByName(username : string) : Observable<User>
-  {
-    let queryParams = new HttpParams().append('username',username);
-    this.http.get(environment.baseApiUrl+'/api/User/GetUserByName',{params: queryParams}).subscribe((res)=>{
+  GetUserByName(username: string): Observable<User> {
+    let queryParams = new HttpParams().append('username', username);
+    this.http.get(environment.baseApiUrl + '/api/User/GetUserByName', { params: queryParams }).subscribe((res) => {
       return res;
     },
-    (error)=>{
-      console.log(JSON.stringify(error));
-    });
+      (error) => {
+        console.log(JSON.stringify(error));
+      });
     return null;
   }
 
-  GetUser(userId : number): Observable<User>{
-    let queryParams = new HttpParams().append('userId',userId);
-    return this.http.get<User>(environment.baseApiUrl+'/api/User/GetUser',{params: queryParams});
+  GetUser(userId: number): Observable<User> {
+    let queryParams = new HttpParams().append('userId', userId);
+    return this.http.get<User>(environment.baseApiUrl + '/api/User/GetUser', { params: queryParams });
+  }
+
+  UpdateUser(userreq: User): Observable<User> {
+    const formData = new FormData();
+    for (const prop in userreq) {
+      if (userreq.hasOwnProperty(prop)) {
+        formData.append(prop, userreq[prop]);
+      }
+    }
+    return this.http.put<User>(environment.baseApiUrl+'/api/User/UpdateUser',formData);
   }
 
   //Image Handling
 
-  GetImage(Imagepath : string) : Observable<Blob>
-  {
-    return this.http.get(environment.baseApiUrl+`/api/User/Images/${Imagepath}`,{responseType : 'blob'});
+  GetImage(Imagepath: string): Observable<Blob> {
+    return this.http.get(environment.baseApiUrl + `/api/User/Images/${Imagepath}`, { responseType: 'blob' });
   }
 }
