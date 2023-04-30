159096824d9b108a9caf437fe52bfd1750f60b82
Judes0n <judesonjoy111@gmail.com>
2023-04-30 20:14:45 +0530

    Minor Changes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Feedback } from 'src/app/models/feedback';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-
+  contactForm : FormGroup = new FormGroup({feed : new FormControl(null,Validators.required)});
+  constructor(private http: HttpClient){}
+  submit()
+  {
+    let feedback : Feedback;
+    feedback = {
+      fid : 0,
+      feed : this.contactForm.get("feed").value
+    };
+    this.http.post(environment.baseApiUrl+'/api/User/Feed',feedback).subscribe(res=>{
+      alert("Send Feedback Successfully!!");
+      this.contactForm.get('feed').setValue("");
+    });
+  }
 }
